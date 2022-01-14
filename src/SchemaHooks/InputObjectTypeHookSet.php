56a40c8c8779ac7309a1d0660bc47b4df33eac35
leoloso <leo@getpop.org>
2022-01-14 06:56:43 +0000

    Merge pull request #1356 from leoloso/Create-root-wp,-move-there-TranslationWP,-HooksWP-and-RoutingWP
    Create RootWP, move there TranslationWP, HooksWP and RoutingWP

--- src/SchemaHooks/InputObjectTypeHookSet.php
+++ src/SchemaHooks/InputObjectTypeHookSet.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace PoPWPSchema\Media\SchemaHooks;
 
+use PoP\Root\App;
 use PoP\ComponentModel\TypeResolvers\InputObjectType\HookNames;
 use PoP\ComponentModel\TypeResolvers\InputObjectType\InputObjectTypeResolverInterface;
 use PoP\ComponentModel\TypeResolvers\InputTypeResolverInterface;
 use PoP\ComponentModel\TypeResolvers\ScalarType\StringScalarTypeResolver;
-use PoP\BasicService\AbstractHookSet;
+use PoP\Root\Hooks\AbstractHookSet;
 use PoPSchema\Media\TypeResolvers\InputObjectType\MediaItemByInputObjectTypeResolver;
 use PoPSchema\SchemaCommons\FilterInputProcessors\FilterInputProcessor;
 
@@ -27,19 +28,19 @@ class InputObjectTypeHookSet extends AbstractHookSet
 
     protected function init(): void
     {
-        $this->getHooksAPI()->addFilter(
+        App::addFilter(
             HookNames::INPUT_FIELD_NAME_TYPE_RESOLVERS,
             [$this, 'getInputFieldNameTypeResolvers'],
             10,
             2
         );
-        $this->getHooksAPI()->addFilter(
+        App::addFilter(
             HookNames::INPUT_FIELD_DESCRIPTION,
             [$this, 'getInputFieldDescription'],
             10,
             3
         );
-        $this->getHooksAPI()->addFilter(
+        App::addFilter(
             HookNames::INPUT_FIELD_FILTER_INPUT,
             [$this, 'getInputFieldFilterInput'],
             10,
